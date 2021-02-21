00552c6cc8fd613c2604431f9a9ecaee0926999f
Austin Perez <austin.perez760@gmail.com>
2021-02-20 19:04:19 -0800

    completed local storage with a for loop to grab item, created array fo  text field begin and end times

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ $("button").click(function () {
   var addVal = $(this).prev().val();
   localStorage.setItem(addValID, addVal);
 });
+// Collecting local storage data to serve to display
+for (i = 0; i < row.length; i++) {
+  $("#" + i).val(localStorage.getItem(i));
+}
 
 var arayTimes = [
   { textarea: $("#8"), begin: "08:00:00", end: "08:59:59" },
@@ -25,3 +29,18 @@ var arayTimes = [
   { textarea: $("#4"), begin: "08:00:00", end: "08:59:59" },
   { textarea: $("#5"), begin: "08:00:00", end: "08:59:59" },
 ];
+
+colorShift();
+function colorShift() {
+  for (i = 0; i < arayTimes.length; i++) {
+    var colorPrior = moment(arayTimes[i].begin, timeFormat);
+    var colorAfter = moment(arayTimes[i].end, timeFormat);
+    if (moment().isBetween(colorPrior, colorAfter)) {
+      arayTimes[i].textarea.addClass("present");
+    } else if (moment().isBefore(colorPrior)) {
+      arayTimes[i].textarea.addClass("future");
+    } else if (moment().isAfter(colorAfter)) {
+      arayTimes[i].textarea.addClass("past");
+    }
+  }
+}
